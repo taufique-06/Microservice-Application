d3544508c5ebcb6ff3ef320cb09ad39a1278d931
Taufique Azad <Taufique.Azad@mountainwarehouse.com>
2022-11-25 14:02:24 +0000

    Added model and db context(In-Memory)

---
+++ PlatformService/Data/PlatfromDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatfromDbContext:DbContext
+    {
+        public PlatfromDbContext(DbContextOptions<PlatfromDbContext> db) : base(db)
+        {
+
+        }
+
+       public DbSet<Platform> Platforms { get; set; }
+    }
+}
